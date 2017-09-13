820b4185e4038ca1728a79a2339ba9b0b56ce205
Paul Dantam <pauldantam@Pauls-MacBook-Pro.local>
2017-09-13 13:02:40 -0500

    Add NoTransportException Class

--- src/com/TheIronYard/Main.java
+++ src/com/TheIronYard/Main.java
@@ -1,8 +1,29 @@
 package com.TheIronYard;
 
+import java.time.LocalDateTime;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        EmailNotification eNote = new EmailNotification(LocalDateTime.now(), "job", "get a jerb",
+                                                                                 "Me", "wife" );
+
+
+        eNote.transport();
+
+
+        TextNotification tNote = new TextNotification(LocalDateTime.now(), "wake up", "now",
+                                                                                "me", "alarm");
+
+
+        tNote.transport();
+
+
+
+
+
+
     }
+
 }
---
+++ src/com/TheIronYard/NoTransportException.java
@@ -0,0 +1,4 @@
+package com.TheIronYard;
+
+public class NoTransportException extends Exception {
+}
